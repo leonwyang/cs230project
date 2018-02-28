3b74319fa4b720bbef3237fb749a612995a4735a
Ubuntu <ubuntu@ip-172-31-44-133.ec2.internal>
2018-02-28 12:40:23 +0000

    script for combined loss

---
+++ SRGAN-tensorflow/train_SRResnet_MSE_VGG54_5.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES=0 python main.py \
+    --output_dir ./experiment_SRResnet_MSE_VGG54_5/ \
+    --summary_dir ./experiment_SRResnet_MSE_VGG54_5/log/ \
+    --mode train \
+    --is_training True \
+    --task SRResnet \
+    --batch_size 16 \
+    --flip True \
+    --random_crop True \
+    --crop_size 24 \
+    --input_dir_LR ./data/LR_train/ \
+    --input_dir_HR ./data/HR_train/ \
+    --num_resblock 16 \
+    --name_queue_capacity 4096 \
+    --image_queue_capacity 4096 \
+    --perceptual_mode MSE_VGG54 \
+    --queue_thread 4 \
+    --ratio 0.001 \
+    --combined_mse_scaling 5.0 \
+    --learning_rate 0.0001 \
+    --decay_step 100000 \
+    --decay_rate 0.1 \
+    --stair False \
+    --beta 0.9 \
+    --max_iter 1000000 \
+    --save_freq 20000
+
